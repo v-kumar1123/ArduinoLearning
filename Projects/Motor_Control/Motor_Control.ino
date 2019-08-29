59b0200270aceab70bab3e0dcc7d6924dce444b2
OTHSCS164 <OTHSCS164@KATYISD.ORG>
2019-08-29 13:39:51 -0500

    Fix Motor Circuit

---
+++ Projects/Motor_Control/Motor_Control.ino
@@ -0,0 +1,30 @@
+/***************************************/
+const int motorIn1 = 9; // the one pin of the motor attach to pin 9
+const int motorIn2 = 10; // the another pin of the motor attach to pin 10
+/***************************************/
+void setup()
+{
+pinMode(motorIn1,OUTPUT); //initialize the motorIn1 pin as output 
+pinMode(motorIn2,OUTPUT); //initialize the motorIn2 pin as output 
+}
+/****************************************/
+void loop()
+{
+clockwise(200); //rotate clockwise 
+delay(1000); //wait for a second
+counterclockwise(200); //rotate counterclockwise
+delay(1000); //wait for a second
+}
+/****************************************/
+//The function to drive motor rotate clockwise
+void clockwise(int Speed)
+{
+analogWrite(motorIn1,Speed); //set the speed of motor
+analogWrite(motorIn2,0); //stop the motorIn2 pin of motor
+}
+//The function to drive motor rotate counterclockwise
+void counterclockwise(int Speed)
+{
+analogWrite(motorIn1,0); //stop the motorIn1 pin of motor
+analogWrite(motorIn2,Speed); //set the speed of motor
+}
